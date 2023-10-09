6481713f94b07918bc32d01f8020a5e478b015ad
tyqqj0 <tyqqj0@163.com>
2023-10-09 09:33:36 +0800

    改问题

--- main_train.py
+++ main_train.py
@@ -37,7 +37,7 @@ parser.add_argument("--log_dir", type=str, default="./runs", help="log dir")
 # parser.add_argument("--artifact_dir", type=str, default="./artifacts", help="artifact dir")
 parser.add_argument("--tag_id", type=str, default=None, help="tag id, ***commanded to set***")
 parser.add_argument("--log_frq", type=int, default=100, help="log frequency")
-parser.add_argument("--save_frq", type=int, default=50, help="save frequency, disabled in test and val")  # 目前是控制val的频率
+parser.add_argument("--save_frq", type=int, default=50, help="save frequency, disabled in test and val")  # 目前是控制在弄val的频率
 
 parser.add_argument("--user_name", type=str, default="tyqqj", help="user name")
 
