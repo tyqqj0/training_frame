6b437449ec818147a816e7e6bfdde6dd14e5ee52
tyqqj0 <tyqqj0@163.com>
2023-10-31 13:32:43 +0800

    render有问题

--- utils/render.py
+++ utils/render.py
@@ -33,14 +33,18 @@ import matplotlib.pyplot as plt
 import vtk
 from vtk.util import numpy_support
 import itk
+# from mayavi import mlab
+
+# # 假设 `voxels` 是你的体素数据
+# mlab.contour3d(voxels)
+# mlab.show()
 
 from sklearn.preprocessing import MinMaxScaler
 
 camera_pos = [0, -1, 0]
 
 
-
-def checker(vtk_image, n = 64):
+def checker(vtk_image, n=64):
     # 转化成numpy
     arr = numpy_support.vtk_to_numpy(vtk_image.GetPointData().GetScalars())
     # 抽取其中的一个层
@@ -57,6 +61,7 @@ def checker(vtk_image, n = 64):
     # 使用 matplotlib 进行可视化
     plt.imshow(slice_arr, cmap='gray', interpolation='nearest')
     plt.show()
+
 
 def read_mha_to_vtk(path):
     print("loading", path)
@@ -86,8 +91,8 @@ def read_mha_to_vtk(path):
 
     return vtk_image
 
-def render_vtk_image(vtk_image, camera_position=(0, -1, 0), focal_point=(0, 0, 0), view_up=(0, 0, 1)):
 
+def render_vtk_image(vtk_image, camera_position=(0, -10, 0), focal_point=(0, 0, 0), view_up=(0, 0, 1)):
     print("rendering")
     # 创建渲染器和渲染窗口
     renderer = vtk.vtkRenderer()
@@ -119,16 +124,24 @@ def render_vtk_image(vtk_image, camera_position=(0, -1, 0), focal_point=(0, 0, 0
     volumeProperty.SetColor(volumeColor)
     volumeProperty.SetScalarOpacity(volumeScalarOpacity)
     volumeProperty.SetGradientOpacity(volumeGradientOpacity)
-    volumeProperty.SetInterpolationTypeToLinear()
+    volumeProperty.SetInterpolationTypeToLinear()  # 线性插值
 
     volume = vtk.vtkVolume()
     volume.SetMapper(volumeMapper)
     volume.SetProperty(volumeProperty)
 
+    # mlab.contour3d(voume)
+    # mlab.show()
+
+    # plt.imshow(volume[50, :, :])  # 显示第 50 层
+    # # 显示第 50 层
+    # plt.show()
+
     # 添加体渲染到渲染器
     renderer.AddVolume(volume)
 
     # 开始渲染
+    print("rendering")
     renderWindow.Render()
 
     # 获取渲染的图像
@@ -137,6 +150,51 @@ def render_vtk_image(vtk_image, camera_position=(0, -1, 0), focal_point=(0, 0, 0
     windowToImageFilter.Update()
 
     return windowToImageFilter.GetOutput()
+
+
+def vtk_image_to_numpy_3d(vtk_image):
+    # 获取vtkImageData的宽度和高度
+    dims = vtk_image.GetDimensions()
+
+    # 将vtkImageData转换为NumPy数组
+    vtk_array = vtk_image.GetPointData().GetScalars()
+    numpy_array = numpy_support.vtk_to_numpy(vtk_array)
+
+    # 将NumPy数组的形状调整为图像的形状
+    numpy_array = numpy_array.reshape(dims[2], dims[1], dims[0])
+
+    return numpy_array
+
+
+def show_image_with_plt(vtk_image):
+    # 将vtkImageData转换为NumPy数组
+    numpy_array = vtk_to_numpy(vtk_image)
+
+    # 如果图像是RGB图像，则需要将颜色通道放在最后
+    if numpy_array.shape[2] == 3:
+        numpy_array = np.flip(numpy_array, axis=2)
+
+    # 使用matplotlib来显示图像
+    plt.imshow(numpy_array)
+    plt.show()
+
+def vtk_to_numpy(vtk_image):
+    # 获取vtkImageData中的点数据
+    point_data = vtk_image.GetPointData()
+
+    # 获取vtkImageData中的标量数据
+    scalar_data = point_data.GetScalars()
+
+    # 将vtkArray转换为NumPy数组
+    numpy_array = numpy_support.vtk_to_numpy(scalar_data)
+
+    # 获取图像的维度
+    dims = vtk_image.GetDimensions()
+
+    # 重塑NumPy数组以匹配图像的维度
+    numpy_array = numpy_array.reshape(dims[1], dims[0], -1)
+
+    return numpy_array
 
 
 def show_vtk_image(vtk_image):
@@ -157,15 +215,33 @@ def show_vtk_image(vtk_image):
     iren.Start()
 
 
+# def show_image_with_plt(vtk_image, n=64):
+#     # 将vtkImageData转换为NumPy数组
+#     numpy_array = vtk_image_to_numpy(vtk_image)
+#
+#     # 如果图像是RGB图像，则需要将颜色通道放在最后
+#     if numpy_array.shape[2] == 3:
+#         numpy_array = np.flip(numpy_array, axis=2)
+#
+#     # 选择一个层进行可视化
+#     # if numpy_array.shape[0] <= n:
+#     # img = numpy_array[n, :, :]
+#
+#     # 使用matplotlib来显示图像
+#     plt.imshow(img)
+#     plt.show()
+
+
 if __name__ == '__main__':
     # 读取 .mha 文件
     vtk_image = read_mha_to_vtk("D:\\Data\\test\\249_label.mha")
 
     # 渲染并保存图像
-    windowed_image = render_vtk_image(vtk_image)
+    windowed_image = render_vtk_image(vtk_image, [0, -1, 0], [0, 1, 0], [0, 0, 1])
     checker(vtk_image)
     # 显示
-    show_vtk_image(windowed_image)
+    # show_vtk_image(windowed_image)
+    show_image_with_plt(windowed_image)
     # writer = vtk.vtkPNGWriter()
     # writer.SetFileName("C:\\Users\\tyqqj\\Desktop\\test.png")
     # writer.SetInputData(windowed_image)
